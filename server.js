726858661dd99a12a46ea397c9da730e392e8ba0
parinyad123 <sci.equ@gmail.com>
2019-03-14 09:50:58 +0700

    initial express server with routes files

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
 const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -13,6 +17,12 @@ mongoose
 
 const app = express();
 app.get('/',(req,res) => res.send('Hellow World--'));
+
+// Use Routes
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
+
 const port = process.env.POST || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
