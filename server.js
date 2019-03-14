d56d0966ad8c5459c03730a4b9edff24c6238d39
parinyad123 <sci.equ@gmail.com>
2019-03-14 08:40:09 +0700

    add mongo

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
 const app = express();
 app.get('/',(req,res) => res.send('Hellow World--'));
 const port = process.env.POST || 5000;
-app.listen(port, () => console.log('Server running on port ${port}'));
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+// ''
+// ""
+// ``
+
+// "asdasdsad'asdasd'" => asdasdsad'asdasd'
+// 'sdfsdf"fsdfsdf"' => sdfsdf"fsdfsdf" 
+
+// 'asdaasd'+'asdasdas'
+// `sadasdasd"" '' ${port}`
